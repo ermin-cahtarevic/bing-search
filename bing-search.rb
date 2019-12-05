f1e3cf8d73eaa802459c8a3996dfd34374b235d4
Vashira <vashimgs@gmail.com>
2019-12-05 17:47:49 +0000

    added way to get the user input and outputting the response

--- bing-search.rb
+++ bing-search.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 
 class Bing
-  def self.search (query)
-    response = RestClient.get 'https://www.bing.com/search?', {params: {'q=' => query}}
+  def self.search(query)
+    response = RestClient.get 'https://www.bing.com/search?', params: { 'q=' => query }
+
+    puts response.body
   end
 end
+
+print 'search input' 
+input_query = gets.chomp
+Bing.search(input_query)
